5bd5c98c7b96034b7bc4f969104929438d1d6210
minhan18 <minhankyaw.developer2015@gmail.com>
2020-02-20 16:04:42 +0630

    added new files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDia8S59tQF9EoI740y1B47uk7Ft1hyV5Y",
+    authDomain: "angularcrud-c65ef.firebaseapp.com",
+    databaseURL: "https://angularcrud-c65ef.firebaseio.com",
+    projectId: "angularcrud-c65ef",
+    storageBucket: "angularcrud-c65ef.appspot.com",
+    messagingSenderId: "88831486739",
+    appId: "1:88831486739:web:554c50ac5bcc12b51588e6",
+    measurementId: "G-HQ4RXEG93G"
+  }
 };
 
 /*
